18951b83c22d1d50ed453181de6ba88d3a1c3e07
super-vicc <super-vicc@ukr.net>
2017-06-29 17:17:50 +0300

    Edition for Shopware version which haven't attributes in sCustomPage tpl variable

--- Controllers/Widgets/OssCustomModel.php
+++ Controllers/Widgets/OssCustomModel.php
@@ -9,20 +9,25 @@ class Shopware_Controllers_Widgets_OssCustomModel extends Enlight_Controller_Act
         $perPage = (int) $this->Request()->getParam(
             'perPage', Shopware()->Config()->getByNamespace('OssCustomModel', 'amount')
         );
-        $predefined = (array)$this->Request()->getParam('predefined', []);
-        $customBannerId = (int)$this->Request()->getParam('customBannerId', 0);
+        $customId = (int)$this->Request()->getParam('customId', 0);
+        $customBannerId = $predefined = $banner = null;
+
+        if($customId){
+            $attributes = Shopware()->Db()->fetchRow('SELECT * FROM `s_cms_static_attributes` WHERE `cmsStaticID`=?', [$customId]);
+            $predefined = explode('|', trim($attributes['customize'], '|'));
+            $customBannerId = $attributes['custombanner'];
 
 //        Fetch Banner
-        $banner = null;
-        try {
-            $context = Shopware()->Container()->get('shopware_storefront.context_service')->getShopContext();
-            if ($customBannerId) {
-                $media = Shopware()->Container()->get('shopware_storefront.media_service')->get(
-                    $customBannerId, $context
-                );
-                $banner = Shopware()->Container()->get('legacy_struct_converter')->convertMediaStruct($media);
-            }
-        }catch(\Exception $e){}
+            try {
+                if ($customBannerId) {
+                    $context = Shopware()->Container()->get('shopware_storefront.context_service')->getShopContext();
+                    $media = Shopware()->Container()->get('shopware_storefront.media_service')->get(
+                        $customBannerId, $context
+                    );
+                    $banner = Shopware()->Container()->get('legacy_struct_converter')->convertMediaStruct($media);
+                }
+            }catch(\Exception $e){}
+        }
 
         $this->View()->ossCustomModels = $this->getAll($perPage, $predefined);
         $this->View()->ossCustomBanner = $banner;
--- Resources/views/frontend/custom/index.tpl
+++ Resources/views/frontend/custom/index.tpl
@@ -3,8 +3,8 @@
 {* content wrapper *}
 {block name="frontend_index_content" prepend}
     {block name="frontend_oss_custom_model"}
-        <div class="container {$ossPosition}">
-            {action module=widgets controller=OssCustomModel customBannerId=$ossBanner}
+        <div class="container">
+            {action module=widgets controller=OssCustomModel customId=$sCustomPage.id}
         </div>
     {/block}
 {/block}
